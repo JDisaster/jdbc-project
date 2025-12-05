8e15b65135f6284d98ac03690fe50bc7e50fcf74
JDisaster <fork868788@gmail.com>
2025-12-04 16:45:25 -0800

    added some database manipulation methods, including login method logic

--- demo/schema.sql
+++ demo/schema.sql
@@ -3,8 +3,10 @@ USE studentdb;
 
 CREATE TABLE IF NOT EXISTS students(
     studentID INT AUTO_INCREMENT PRIMARY KEY,
-    studentName VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
+    studentName VARCHAR(200) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    passwordHash VARCHAR(255) NOT NULL,
+    major VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS courses(
--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import com.example.mysql.Database;
 
 /**
  * JavaFX App
